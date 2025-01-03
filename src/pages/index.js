535cb1cbb7a8c45714fead280501c76be4395215
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2025-01-03 17:30:55 +0100

    link mistake

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/introduzione/intro">
+            to="/introduzione/intro">
             Vai alla Reference
           </Link>
         </div>
